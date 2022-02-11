ca884c287d19b13ea006590ddf54f3459520eb59
Vinicius Mattei <vinicius.mattei@etu.univ-grenoble-alpes.fr>
2022-02-11 11:50:15 +0100

    premiere version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TodolistService, TodoList } from './todolist.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'l3m-tpX-todolist-angular-y2022';
+  todoList: TodoList | any;
+  todoListObs!: Observable<TodoList>;
+
+  constructor(public service: TodolistService) {
+    this.todoListObs = service.observable;
+  }
+
+  ngOnInit() {
+    this.service.observable
+      .subscribe( reponse => {
+        this.todoList = reponse;
+        console.log(reponse);
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { TodolistService } from './todolist.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    TodolistService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
